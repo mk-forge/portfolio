fa494cd8041767c8f286f5be7f3c43d4b02d3508
mk-forge <misakantor@gmail.com>
2026-09-19 19:43:36 +0200

    Switch readmes/screenshots to URLs, update homepage and projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </svelte:head>
 
 <script lang="ts">
-  import { CLogo, CppLogo, CSharpLogo, GradleLogo, DoxygenLogo, PostmanLogo, BootstrapLogo, OpenGLLogo, PrismaLogo, SwigLogoLight, SwigLogoDark, ExpressLogo, BlazorLogo, PostgreSQLLogo, SQLiteLogo, NextJSLogo, IntelliJIDEALogo, BashLogo, BatchLogo, JavaScriptLogo, TypeScriptLogo, NodeJSLogo, MakeLogo, DatabaseIcon } from '$lib/assets/icons';
+  import { CLogo, CppLogo, CSharpLogo, GradleLogo, DoxygenLogo, PostmanLogo, BootstrapLogo, OpenGLLogo, PrismaLogo, SwigLogoLight, SwigLogoDark, ExpressLogo, BlazorLogo, PostgreSQLLogo, SQLiteLogo, NextJSLogo, IntelliJIDEALogo, BashLogo, BatchLogo, JavaScriptLogo, TypeScriptLogo, NodeJSLogo, MakeLogoLight, MakeLogoDark, ViteLogoLight, ViteLogoDark, DatabaseIcon, PythonLogo, CordovaLogo, IonicLogo, EmscriptenLogoLight, EmscriptenLogoDark, WiresharkLogo, PacketTracerLogo, VirtualBoxLogo, VmwareLogo, SvelteLogo, TampermonkeyLogo, FormspreeLogo, JavaLogo, ReactLogo, DotnetLogo, DjangoLogo, WebAssemblyLogo, VisualStudioLogo, VisualStudioCodeLogo, GitLogo, GitLabLogo, GitHubLogo, CloudflareWorkersLogo, NetlifyLogo, MavenLogo, SonarQubeLogo, ESLintLogo, PuttyLogo, WindowsLogo, AndroidLogo, LinuxLogo } from '$lib/assets/icons';
   import '$lib/styles/home.css';
 </script>
 
@@ -27,10 +27,9 @@
             <span class="skill-item"><img src={TypeScriptLogo} alt="TypeScript" class="custom-logo"/> TypeScript <span class="tag">širší</span></span>
             <span class="skill-item"><img src={DatabaseIcon} alt="SQL" class="custom-logo"/> SQL <span class="tag">širší</span></span>
             <span class="skill-item"><img src={CLogo} alt="C" class="custom-logo"/> C <span class="tag">základní</span></span>
-            <span class="skill-item"><i class="devicon-python-plain devicon-logo"></i> Python <span class="tag">základní</span></span>
-            <span class="skill-item"><i class="devicon-java-plain devicon-logo"></i> Java <span class="tag">základní</span></span>
+            <span class="skill-item"><img src={PythonLogo} alt="Python" class="custom-logo"/> Python <span class="tag">základní</span></span>
+            <span class="skill-item"><img src={JavaLogo} alt="Java" class="custom-logo"/> Java <span class="tag">základní</span></span>
             <span class="skill-item"><img src={DatabaseIcon} alt="PL/SQL" class="custom-logo"/> PL/SQL <span class="tag">základní</span></span>
-            <span class="skill-item"><i class="devicon-svelte-plain devicon-logo"></i> Svelte <span class="tag">základní</span></span>
             <span class="skill-item"><img src={BatchLogo} alt="Batch" class="custom-logo"/> Batch <span class="tag">základní</span></span>
             <span class="skill-item"><img src={BashLogo} alt="Bash" class="custom-logo"/> Bash <span class="tag">základní</span></span>
           </div>
@@ -41,18 +40,22 @@
         <div class="skills-section">
           <h3 class="heading"><span class="heading-prefix">></span> Technologie & frameworky</h3>
           <div class="skill-items">
-            <span class="skill-item"><i class="devicon-svelte-plain devicon-logo"></i> SvelteKit</span>
-            <span class="skill-item"><i class="devicon-react-original devicon-logo"></i> React</span>
+            <span class="skill-item"><img src={SvelteLogo} alt="Svelte" class="custom-logo"/> SvelteKit</span>
+            <span class="skill-item"><img src={ReactLogo} alt="React" class="custom-logo"/> React</span>
             <span class="skill-item"><img src={NextJSLogo} alt="Next.js" class="custom-logo"/> Next.js</span>
-            <span class="skill-item"><i class="devicon-dot-net-plain devicon-logo"></i> .NET</span>
+            <span class="skill-item"><img src={DotnetLogo} alt=".NET" class="custom-logo"/> .NET</span>
             <span class="skill-item"><img src={BlazorLogo} alt="Blazor" class="custom-logo"/> Blazor</span>
-            <span class="skill-item"><img src={NodeJSLogo} alt="NodeJS" class="custom-logo"/> Node.js</span>
+            <span class="skill-item"><img src={NodeJSLogo} alt="Node.js" class="custom-logo"/> Node.js</span>
             <span class="skill-item"><img src={ExpressLogo} alt="Express" class="custom-logo"/> Express</span>
+            <span class="skill-item"><img src={DjangoLogo} alt="Django" class="custom-logo"/> Django</span>
             <span class="skill-item"><img src={PostgreSQLLogo} alt="PostgreSQL" class="custom-logo"/> PostgreSQL</span>
             <span class="skill-item"><img src={SQLiteLogo} alt="SQLite" class="custom-logo"/> SQLite</span>
             <span class="skill-item"><img src={PrismaLogo} alt="Prisma" class="custom-logo"/> Prisma</span>
             <span class="skill-item"><img src={OpenGLLogo} alt="OpenGL" class="custom-logo"/> OpenGL</span>
+            <span class="skill-item"><img src={WebAssemblyLogo} alt="WebAssembly" class="custom-logo"/> WebAssembly</span>
             <span class="skill-item"><img src={BootstrapLogo} alt="Bootstrap" class="custom-logo"/> Bootstrap</span>
+            <span class="skill-item"><img src={IonicLogo} alt="Ionic" class="custom-logo"/> Ionic</span>
+            <span class="skill-item"><img src={CordovaLogo} alt="Cordova" class="custom-logo"/> Cordova</span>
           </div>
         </div>
       </div>
@@ -61,27 +64,31 @@
         <div class="skills-section">
           <h3 class="heading"><span class="heading-prefix">></span> Vývojová prostředí & nástroje</h3>
           <div class="skill-items">
-            <span class="skill-item"><i class="devicon-visualstudio-plain devicon-logo"></i> Visual Studio</span>
-            <span class="skill-item"><i class="devicon-vscode-plain devicon-logo"></i> Visual Studio Code</span>
+            <span class="skill-item"><img src={VisualStudioLogo} alt="Visual Studio" class="custom-logo"/> Visual Studio</span>
+            <span class="skill-item"><img src={VisualStudioCodeLogo} alt="Visual Studio Code" class="custom-logo"/> Visual Studio Code</span>
             <span class="skill-item"><img src={IntelliJIDEALogo} alt="IntelliJ IDEA" class="custom-logo"/> IntelliJ IDEA</span>
-            <span class="skill-item"><i class="devicon-git-plain devicon-logo"></i> Git</span>
-            <span class="skill-item"><i class="devicon-gitlab-plain devicon-logo"></i> GitLab</span>
-            <span class="skill-item"><i class="devicon-github-original devicon-logo"></i> GitHub</span>
-            <span class="skill-item"><i class="devicon-github-original devicon-logo"></i> GitHub Pages</span>
-            <span class="skill-item"><i class="devicon-netlify-plain devicon-logo"></i> Netlify</span>
-            <span class="skill-item"><img src={MakeLogo} alt="Make" class="custom-logo logo-size"/> GNU Make</span>
-            <span class="skill-item"><i class="devicon-maven-plain devicon-logo"></i> Apache Maven</span>
+            <span class="skill-item"><img src={TampermonkeyLogo} alt="Tampermonkey" class="custom-logo"/> Tampermonkey</span>
+            <span class="skill-item"><img src={GitLogo} alt="Git" class="custom-logo"/> Git</span>
+            <span class="skill-item"><img src={GitLabLogo} alt="GitLab" class="custom-logo"/> GitLab</span>
+            <span class="skill-item"><img src={GitHubLogo} alt="GitHub" class="custom-logo"/> GitHub</span>
+            <span class="skill-item"><img src={CloudflareWorkersLogo} alt="Cloudflare Workers" class="custom-logo"/> Cloudflare Workers</span>
+            <span class="skill-item"><img src={NetlifyLogo} alt="Netlify" class="custom-logo"/> Netlify</span>
+            <span class="skill-item"><img src={MakeLogoLight} alt="Make" class="logo logo-light"/><img src={MakeLogoDark} alt="Make" class="logo logo-dark"/> Make</span>
+            <span class="skill-item"><img src={ViteLogoLight} alt="Vite" class="logo logo-light"/><img src={ViteLogoDark} alt="Vite" class="logo logo-dark"/> Vite</span>
+            <span class="skill-item"><img src={MavenLogo} alt="Maven" class="custom-logo"/> Maven</span>
             <span class="skill-item"><img src={GradleLogo} alt="Gradle" class="custom-logo"/> Gradle</span>
             <span class="skill-item"><img src={DoxygenLogo} alt="Doxygen" class="custom-logo logo-size"/> Doxygen</span>
-            <span class="skill-item"><i class="devicon-sonarqube-original devicon-logo"></i> SonarQube</span>
-            <span class="skill-item"><i class="devicon-eslint-plain devicon-logo"></i> ESLint</span>
-            <span class="skill-item swig-item">
-              <img src={SwigLogoLight} alt="SWIG" class="swig-logo swig-logo-light"/>
-              <img src={SwigLogoDark} alt="SWIG" class="swig-logo swig-logo-dark"/>
-              SWIG
-            </span>
+            <span class="skill-item"><img src={SonarQubeLogo} alt="SonarQube" class="custom-logo logo-size"/> SonarQube</span>
+            <span class="skill-item"><img src={ESLintLogo} alt="ESLint" class="custom-logo"/> ESLint</span>
+            <span class="skill-item"><img src={SwigLogoLight} alt="SWIG" class="swig-logo swig-logo-light"/><img src={SwigLogoDark} alt="SWIG" class="swig-logo swig-logo-dark"/> SWIG</span>
+            <span class="skill-item"><img src={EmscriptenLogoLight} alt="Emscripten" class="logo logo-light"/><img src={EmscriptenLogoDark} alt="Emscripten" class="logo logo-dark"/> Emscripten</span>
+            <span class="skill-item"><img src={FormspreeLogo} alt="Formspree" class="custom-logo"/> Formspree</span>
             <span class="skill-item"><img src={PostmanLogo} alt="Postman" class="custom-logo"/> Postman</span>
-            <span class="skill-item"><i class="devicon-putty-plain devicon-logo"></i> PuTTY</span>
+            <span class="skill-item"><img src={PuttyLogo} alt="PuTTY" class="custom-logo"/> PuTTY</span>
+            <span class="skill-item"><img src={WiresharkLogo} alt="Wireshark" class="custom-logo"/> Wireshark</span>
+            <span class="skill-item"><img src={PacketTracerLogo} alt="Packet Tracer" class="custom-logo"/> Packet Tracer</span>
+            <span class="skill-item"><img src={VirtualBoxLogo} alt="VirtualBox" class="custom-logo"/> VirtualBox</span>
+            <span class="skill-item"><img src={VmwareLogo} alt="VMware" class="custom-logo"/> VMware</span>
           </div>
         </div>
       </div>
@@ -90,9 +97,9 @@
         <div class="skills-section">
           <h3 class="heading"><span class="heading-prefix">></span> Operační systémy & platformy</h3>
           <div class="skill-items">
-            <span class="skill-item"><i class="devicon-windows8-original devicon-logo"></i> Windows <span class="tag">každodenní</span></span>
-            <span class="skill-item"><i class="devicon-android-plain devicon-logo"></i> Android <span class="tag">každodenní</span></span>
-            <span class="skill-item"><i class="devicon-linux-plain devicon-logo"></i> Linux <span class="tag">občasné</span></span>
+            <span class="skill-item"><img src={WindowsLogo} alt="Windows" class="custom-logo"/> Windows <span class="tag">každodenní</span></span>
+            <span class="skill-item"><img src={AndroidLogo} alt="Android" class="custom-logo"/> Android <span class="tag">každodenní</span></span>
+            <span class="skill-item"><img src={LinuxLogo} alt="Linux" class="custom-logo"/> Linux <span class="tag">občasné</span></span>
           </div>
         </div>
       </div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,26 +3,38 @@
 </svelte:head>
 
 <script lang="ts">
-  import { readmeDataGenRaw, readmeRenderForgeRaw, readmePacXonRaw, readmeElearningRaw, readmeMcRaw, readmePortfolioRaw, readmeCsfdRaw, readmeTableTennisRaw, readmeSetIntersectionRaw, readmeWindowsCleanerRaw } from '$lib/assets/readmes';
-  import { DatabaseIcon, CubeIcon, PacmanIcon, BookOpenIcon, GemIcon, IdCardIcon, FilmIcon, PingPongIcon, SquaresIntersectIcon, BroomIcon, ExternalLinkIcon } from '$lib/assets/icons';
+  import { DatabaseIcon, CubeIcon, PacmanIcon, BookOpenIcon, SquaresIntersectIcon, GemIcon, FilmIcon, PingPongIcon, IdCardIcon, BroomIcon, ExternalLinkIcon } from '$lib/assets/icons';
   import { TabPanelScrollbar } from '$lib/components';
   import { marked } from 'marked';
   import '$lib/styles/projects.css';
 
-  let tabs = [
-    { label: 'Data Collection Generator', icon: DatabaseIcon, repoUrl: 'https://github.com/mk-forge/data-collection-generator', content: marked(readmeDataGenRaw) },
-    { label: 'RenderForge', icon: CubeIcon, repoUrl: 'https://github.com/mk-forge/render-forge', content: marked(readmeRenderForgeRaw) },
-    { label: 'PAC-XON', icon: PacmanIcon, repoUrl: 'https://github.com/mk-forge/pac-xon', content: marked(readmePacXonRaw) },
-    { label: 'E-Learning Platform', icon: BookOpenIcon, repoUrl: 'https://github.com/mk-forge/e-learning-platform', content: marked(readmeElearningRaw) },
-    { label: 'Minecraft Gems Mod', icon: GemIcon, repoUrl: 'https://github.com/mk-forge/minecraft-gems-mod', content: marked(readmeMcRaw) },
-    { label: 'Portfolio', icon: IdCardIcon, repourl: 'https://github.com/mk-forge/portfolio', content: marked(readmePortfolioRaw) },
-    { label: 'ČSFD Extended', icon: FilmIcon, repoUrl: 'https://github.com/mk-forge/csfd-extended', content: marked(readmeCsfdRaw) },
-    { label: 'Table Tennis Manager', icon: PingPongIcon, repoUrl: 'https://github.com/mk-forge/table-tennis-manager', content: marked(readmeTableTennisRaw) },
-    { label: 'Set Intersection', icon: SquaresIntersectIcon, repoUrl: 'https://github.com/mk-forge/set-intersection', content: marked(readmeSetIntersectionRaw) },
-    { label: 'Windows Process Cleaner', icon: BroomIcon, repoUrl: 'https://github.com/mk-forge/windows-process-cleaner', content: marked(readmeWindowsCleanerRaw) }
+  let projects = [
+    { label: 'Data Collection Generator', icon: DatabaseIcon, repoUrl: 'https://github.com/mk-forge/data-collection-generator', readmeUrl: 'https://raw.githubusercontent.com/mk-forge/data-collection-generator/main/README.md' },
+    { label: 'RenderForge', icon: CubeIcon, repoUrl: 'https://github.com/mk-forge/render-forge', readmeUrl: 'https://raw.githubusercontent.com/mk-forge/render-forge/main/README.md' },
+    { label: 'PAC-XON', icon: PacmanIcon, repoUrl: 'https://github.com/mk-forge/pac-xon', readmeUrl: 'https://raw.githubusercontent.com/mk-forge/pac-xon/main/README.md' },
+    { label: 'E-Learning Platform', icon: BookOpenIcon, repoUrl: 'https://github.com/mk-forge/e-learning-platform', readmeUrl: 'https://raw.githubusercontent.com/mk-forge/e-learning-platform/main/README.md' },
+    { label: 'Set Intersection', icon: SquaresIntersectIcon, repoUrl: 'https://github.com/mk-forge/set-intersection', readmeUrl: 'https://raw.githubusercontent.com/mk-forge/set-intersection/main/README.md' },
+    { label: 'Minecraft Gems Mod', icon: GemIcon, repoUrl: 'https://github.com/mk-forge/minecraft-gems-mod', readmeUrl: 'https://raw.githubusercontent.com/mk-forge/minecraft-gems-mod/main/README.md' },
+    { label: 'ČSFD Extended', icon: FilmIcon, repoUrl: 'https://github.com/mk-forge/csfd-extended', readmeUrl: 'https://raw.githubusercontent.com/mk-forge/csfd-extended/main/README.md' },
+    { label: 'Table Tennis Manager', icon: PingPongIcon, repoUrl: 'https://github.com/mk-forge/table-tennis-manager', readmeUrl: 'https://raw.githubusercontent.com/mk-forge/table-tennis-manager/main/README.md' },
+    { label: 'Portfolio', icon: IdCardIcon, repoUrl: 'https://github.com/mk-forge/portfolio', readmeUrl: 'https://raw.githubusercontent.com/mk-forge/portfolio/main/README.md' },
+    { label: 'Windows Process Cleaner', icon: BroomIcon, repoUrl: 'https://github.com/mk-forge/windows-process-cleaner', readmeUrl: 'https://raw.githubusercontent.com/mk-forge/windows-process-cleaner/main/README.md' }
   ];
 
   let activeTab = $state(0);
+  let readmes: Record<string, string> = $state({});
+
+  $effect(() => {
+    projects.forEach(project => {
+      if (readmes[project.repoUrl] == undefined) {
+        fetch(project.readmeUrl)
+          .then(response => response.text())
+          .then(text => {
+            readmes[project.repoUrl] = text;
+          });
+      }
+    });
+  });
 
   function openLinksInNewTab() {
     let links = document.querySelectorAll('.project-content a');
@@ -33,7 +45,11 @@
   }
 
   $effect(() => {
-    openLinksInNewTab();
+    activeTab;
+    
+    if (readmes[projects[activeTab].repoUrl] != undefined) {
+      setTimeout(openLinksInNewTab, 0);
+    }
   });
 </script>
 
@@ -44,11 +60,11 @@
 
     <div class="tabs-container">
       <div class="tab-list">
-        {#each tabs as tab, i}
+        {#each projects as project, i}
           <button type="button" class="tab" class:active={activeTab == i} onclick={() => activeTab = i}>
-            <img src={tab.icon} alt="" class="tab-icon"/>
-            <span>{tab.label}</span>
-            <a href={tab.repoUrl} target="_blank" rel="noopener noreferrer" class="repo-link-btn" title="Otevřít repozitář" onclick={(e) => e.stopPropagation()} tabindex="-1">
+            <img src={project.icon} alt="" class="tab-icon"/>
+            <span>{project.label}</span>
+            <a href={project.repoUrl} target="_blank" rel="noopener noreferrer" class="repo-link-btn" title="Otevřít repozitář" onclick={(e) => e.stopPropagation()} tabindex="-1">
               <img src={ExternalLinkIcon} alt="" class="tab-icon repo-icon"/>
             </a>
           </button>
@@ -58,7 +74,11 @@
       <TabPanelScrollbar>
         <div class="tab-panel">
           <div class="project-content">
-            {@html tabs[activeTab].content}
+            {#if readmes[projects[activeTab].repoUrl] != undefined}
+              {@html marked(readmes[projects[activeTab].repoUrl])}
+            {:else}
+              <p>Načítám…</p>
+            {/if}
           </div>
         </div>
       </TabPanelScrollbar>
